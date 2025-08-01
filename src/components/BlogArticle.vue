fe71b15c3171c84e0aa0a03d55b551c090fa1dda
Tienlam03 <tienlam301203@gmail.com>
2025-08-01 09:25:32 +0700

    1

--- src/components/BlogArticle.vue
+++ src/components/BlogArticle.vue
@@ -24,7 +24,6 @@
                 {{ article.date }}
             </span>
         </div>
-<<<<<<< HEAD
         <!-- CONTENT HOLDER -->
         <div class="pt-[45px] transition-all duration-500 relative">
             <ul
@@ -91,78 +90,6 @@
             </div>
         </div>
     </article>
-=======
-      </router-link>
-      <span
-        class="absolute bottom-0 left-0 text-white text-[22px] px-[25px] py-[13px] rounded-tl-none rounded-tr-[25px] flex items-center bg-gradient-to-r from-[#24B7D3] to-[#30EFAD]"
-      >
-        <i class="mr-2 far fa-calendar-alt"></i>
-        {{ article.date }}
-      </span>
-    </div>
-    <!-- CONTENT HOLDER -->
-    <div class="pt-[45px] transition-all duration-500 relative">
-      <ul
-        class="flex flex-wrap items-center font-semibold mb-[10px] pb-[10px] text-[14px] uppercase pxl-item--meta"
-        style="font-family: 'Chakra Petch', sans-serif;"
-      >
-        <li class="flex items-center text-[#6F7F90] font-medium mr-[10px] pr-[10px]">
-          <i class="bootstrap-icons bi-person-circle text-[#2AD2C1] text-[16px] mr-1"></i>
-          <router-link
-            :to="article.authorLink"
-            class="hover:text-[#2AD2C1]"
-          >
-            {{ article.author }}
-          </router-link>
-        </li>
-        <li class="flex items-center mr-[10px] pr-[10px] item--category">
-          <a
-            v-for="(category, index) in article.categories"
-            :key="category.slug"
-            :href="category.link"
-            rel="tag"
-            :class="index > 0 ? '' : 'mr-1'"
-            class="py-[11px] px-[15px] rounded-[25px] bg-[#e1f6f9] text-[#4286f5] inline-block hover:text-[#2AD2C1]"
-          >{{ category.name }}</a>
-        </li>
-        <li class="item-comment">
-          <router-link
-            :to="`/blog/${article.id}`"
-            class="flex items-center text-[#6F7F90] font-medium hover:text-[#2AD2C1]"
-          >
-            <i class="flaticon flaticon-comment text-[#2AD2C1] text-[16px] mr-1"></i>
-            {{ article.commentsCount }} Comments
-          </router-link>
-        </li>
-      </ul>
-      <h2 class="pxl-item--title text-[40px] leading-tight mb-[30px] font-semibold text-[#1D2864]"
-          style="font-family: 'Chakra Petch', sans-serif;">
-        <router-link
-          :to="`/blog/${article.id}`"
-          :title="article.title"
-          class="hover:text-[#2AD2C1]"
-        >{{ article.title }}</router-link>
-      </h2>
-      <div class="mb-[25px] lg:pr-[50px] text-[#6F7F90] pxl-item--excerpt">
-        {{ article.excerpt }}
-      </div>
-      <div class="pxl-item--readmore">
-        <router-link
-          class="btn px-0 text-[#2AD2C1] text-[18px] font-semibold uppercase leading-[47px] hover:text-[#2AD2C1] flex items-center gap-2 bg-transparent shadow-none group"
-          :to="`/blog/${article.id}`"
-        >
-          Xem thêm
-          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" class="ml-1 transition-transform duration-300 group-hover:rotate-45">
-            <path
-              d="M2.04625 14.3533L0.520081 12.8271L10.9853 2.36189H1.6102V0.181641H14.6917V13.2631H12.5114V3.88806L2.04625 14.3533Z"
-              fill="#2AD2C1"
-            ></path>
-          </svg>
-        </router-link>
-      </div>
-    </div>
-  </article>
->>>>>>> 64f8610e0c82a0505d79cee123d543481d1f581f
 </template>
 
 <script setup>
